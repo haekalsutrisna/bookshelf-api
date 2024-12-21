c43b3ffc34fd971821996f4bc9064ac466fd39a1
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2024-12-21 10:00:08 +0700

    update api

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import { addBook, getAllBooks, getBookById, updateBook, deleteBook } from './handlers.js';
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBook,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooks,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookById,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: updateBook,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBook,
+  },
+];
+
+export default routes;
